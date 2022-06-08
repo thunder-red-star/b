7c4b48748d23845355efb5dab457f99a018c2185
ThunderRedStar <wvongphanith30@stuy.edu>
2022-06-07 23:20:10 -0400

    command line parsing

--- 01-primes/np1.c
+++ 01-primes/np1.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
-
-int main() {
+c
+int main(int argc, char *argv[]) {
     int primeCount = 0;
     int startValue = 2;
-    while (primeCount < 1000000) {
+    if (argc < 2) {
+        printf("You didn't provide any arguments!\n");
+        return 1;
+    }
+    int endValue = atoi(argv[1]);
+    if (endValue < 2) {
+        printf("You need to enter a number greater than 1!\n");
+        return 1;
+    }
+    while (primeCount < endValue) {
         int isPrime = 1;
         for (int i = 2; i < startValue; i++) {
             if (startValue % i == 0) {
