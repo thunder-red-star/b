697a77ffac4dc386f5717790e6eaa890a9fb777b
ThunderRedStar <wvongphanith30@stuy.edu>
2022-08-26 21:12:24 -0400

    permutation

---
+++ permutations/permutations.py
@@ -0,0 +1,42 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def nth_perm_helper(array, n):
+    array = sorted(array)
+    # If the array contains 1 element or n is 0, then the nth permutation is the array itself.
+    if len(array) == 1:
+        return [array[0]]
+    else:
+        # Compute the next element's by finding the floor of n divided by the number of possibilities for the next element.
+        # There are len(array) possibilities for the next element.
+        current_array_length = len(array)
+        # We'll find the next element and the resulting n through the next calculation.
+        next_element = n // factorial(current_array_length - 1)
+        next_n = n % factorial(current_array_length - 1)
+        # We'll find the next element by finding the element at the next_element index in the array.
+        next_element = array[next_element]
+        # Build the solution array.
+        solution = [next_element]
+        # Remove the next element from the array.
+        array.remove(next_element)
+        # Recurse on the remaining array.
+        solution.extend(nth_perm_helper(array, next_n))
+        # Return the solution array.
+        return solution
+
+# This function finds the nth permutation of a provided array, without generating all permutations
+def nth_permutation(array, n):
+    return nth_perm_helper(array, n)
+
+def main():
+    array = [1, 2, 3, 4, 5]
+    n = 24
+    print(nth_permutation(array, n))
+    # should print [5, 4, 3, 2, 1]
+
+if __name__ == "__main__":
+    main()
+
---
+++ primes/sieve/bitarray_multithread/sieve.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sieve(int n) {
+    unsigned int* bits = calloc(sizeof(unsigned int), (((n >> 1) - 1) >> 5) + 1);
+    register long int i, j, k;
+    k = 1;
+    #pragma omp parallel for private(i, j)
+    for (i = 3; i < n; i += 2) {
+        if ((bits[((i >> 1) - 1) >> 5] & (1 << (((i >> 1) - 1) % 32))) != 0) continue;
+        for (j = i * i; j < n; j += i << 1) {
+            bits[((j >> 1) - 1) >> 5] |= (1 << (((j >> 1) - 1) % 32));
+        }
+        k++;
+    }
+    free(bits);
+    return k;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("You didn't provide any arguments!\n");
+        return 1;
+    }
+    int endValue = atoi(argv[1]);
+    if (endValue < 2) {
+        printf("You need to enter a number greater than 1!\n");
+        return 1;
+    }
+    printf("Searching for the %dth prime...\n", endValue);
+    int primes = sieve(endValue);
+    printf("There are %d primes below %d.\n", primes, endValue);
+    return 0;
+}
